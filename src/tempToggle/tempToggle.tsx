ab07dee3532cb0fa416aacae07ce29013857e63e
drattylight <mlaux6@outlook.com>
2022-10-04 19:44:11 -0500

    Update temp toggle onclick function name

--- src/tempToggle/tempToggle.tsx
+++ src/tempToggle/tempToggle.tsx
@@ -1,13 +1,13 @@
 import "./tempToggle.css";
 
 interface Props {
-  toggleTemp: () => void;
+  handleToggleTemp: () => void;
 }
 
 const TempToggle = (props: Props): JSX.Element => {
   return (
     <label className="switch">
-      <input type="checkbox" onClick={props.toggleTemp} />
+      <input type="checkbox" onClick={props.handleToggleTemp} />
       <span className="slider round">
         <em className="celsius">C°</em>
         <em className="fahrenheit">F°</em>
