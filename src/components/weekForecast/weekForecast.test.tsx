b478476528e35be914d4b44e0439a29c5417ca4c
drattylight <mlaux6@outlook.com>
2022-10-05 19:25:07 -0500

    Code and test weekForecast component

---
+++ src/components/weekForecast/weekForecast.test.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import partlyCloudyIcon from "../../media/partlyCloudy.png";
+import thunderstormsIcon from "../../media/thunderstorms.png";
+import rainIcon from "../../media/rain.png";
+import WeekForecast from "./weekForecast";
+
+describe("<DayForecast />", () => {
+  test("renders correct day, icon, and temp", () => {
+    render(
+      <WeekForecast
+        dailyForecasts={[
+          {
+            temp: 93,
+            icon: partlyCloudyIcon,
+            date: 1665014400,
+            condition: "Partly Cloudy",
+          },
+          {
+            temp: 32,
+            icon: thunderstormsIcon,
+            date: 1665114400,
+            condition: "Thunderstorms",
+          },
+          {
+            temp: 65,
+            icon: rainIcon,
+            date: 1665129400,
+            condition: "Rain",
+          },
+        ]}
+      />
+    );
+
+    const wednesday = screen.getByText(/wed/i);
+    const wedTemp = screen.getByText(/93°/i);
+    const wedIcon = screen.getByAltText(/partly cloudy/i);
+    expect(wednesday).toBeInTheDocument();
+    expect(wedTemp).toBeInTheDocument();
+    expect(wedIcon).toBeInTheDocument();
+
+    const thursday = screen.getByText(/thu/i);
+    const thuTemp = screen.getByText(/32°/i);
+    const thuIcon = screen.getByAltText(/thunderstorms/i);
+    expect(thursday).toBeInTheDocument();
+    expect(thuTemp).toBeInTheDocument();
+    expect(thuIcon).toBeInTheDocument();
+
+    const friday = screen.getByText(/fri/i);
+    const friTemp = screen.getByText(/32°/i);
+    const friIcon = screen.getByAltText(/rain/i);
+    expect(friday).toBeInTheDocument();
+    expect(friTemp).toBeInTheDocument();
+    expect(friIcon).toBeInTheDocument();
+  });
+});
