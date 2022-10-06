7eb6fb8ac5d2facc8e167a76b6702a59d1db1fc0
drattylight <mlaux6@outlook.com>
2022-10-06 17:45:56 -0500

    Add API tests

--- src/components/cityContainer/cityContainer.test.tsx
+++ src/components/cityContainer/cityContainer.test.tsx
@@ -1,15 +1,43 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import CityContainer from "./cityContainer";
+import server from "../../../mocks/server";
+import networkErrorHandlers from "../../../mocks/networkErrorHandlers";
 
-test("renders current weather information and temperature toggle button", () => {
-  render(<CityContainer tempIsCelsius={false} handleToggleTemp={jest.fn} />);
+describe("<CityContainer />", () => {
+  test("renders temperature toggle button", () => {
+    render(<CityContainer tempIsCelsius={false} handleToggleTemp={jest.fn} />);
 
-  const weatherIcon = screen.getByRole("img");
-  const windText = screen.getByText(/wind:/i);
-  const toggleTempButton = screen.getByRole("checkbox");
+    const toggleTempButton = screen.getByRole("checkbox");
+    expect(toggleTempButton).toBeInTheDocument();
+  });
 
-  expect(weatherIcon).toBeInTheDocument();
-  expect(windText).toBeInTheDocument();
-  expect(toggleTempButton).toBeInTheDocument();
+  test("renders loading state then current weather information", async () => {
+    render(<CityContainer tempIsCelsius={false} handleToggleTemp={jest.fn} />);
+
+    const loadingText = screen.getByText("Loading Weather Data");
+    expect(loadingText).toBeInTheDocument();
+
+    const temp = await screen.findByText(/88°/i);
+    const icon = await screen.findByAltText(/Partly Cloudy/i);
+    const condition = await screen.findByText(/partly cloudy/i);
+    const windSpeed = await screen.findByText(/wind: 1mph/i);
+    expect(temp).toBeInTheDocument();
+    expect(icon).toBeInTheDocument();
+    expect(condition).toBeInTheDocument();
+    expect(windSpeed).toBeInTheDocument();
+  });
+
+  test("renders error state when request fails", async () => {
+    server.use(...networkErrorHandlers);
+    render(<CityContainer tempIsCelsius={false} handleToggleTemp={jest.fn} />);
+
+    const loadingText = screen.getByText("Loading Weather Data");
+    expect(loadingText).toBeInTheDocument();
+
+    const errorText = await screen.findByText(
+      /Failed to retrieve current weather data from API/i
+    );
+    expect(errorText).toBeInTheDocument();
+  });
 });
